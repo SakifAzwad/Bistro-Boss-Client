6326465fa605e74a318d5313e4398d6b505c6180
SakifAzwad <sakifazwad18@gmail.com>
2023-11-21 02:18:08 +0600

    added order food card

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -30,22 +30,22 @@ const Menu = () => {
 
       <MenuCategory
         items={desserts}
-        title="Dessert"
+        title="dessert"
         img={dessertImg}
       ></MenuCategory>
       <MenuCategory
         items={pizza}
-        title="Pizza"
+        title="pizza"
         img={pizzaImg}
       ></MenuCategory>
       <MenuCategory
         items={salad}
-        title="Salad"
+        title="salad"
         img={saladImg}
       ></MenuCategory>
       <MenuCategory
         items={soup}
-        title="Soup"
+        title="soup"
         img={soupImg}
       ></MenuCategory>
     </div>
