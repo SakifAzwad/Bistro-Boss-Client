bc34f4f0934a209fc53811554b82cd76320ab91c
SakifAzwad <sakifazwad18@gmail.com>
2023-11-22 04:38:56 +0600

    started cart feature

--- src/components/FoodCard/FoodCard.jsx
+++ src/components/FoodCard/FoodCard.jsx
@@ -3,7 +3,13 @@
 
 const FoodCard = ({item}) => {
 
-    const {image,price,name,recipe}=item
+    const {image,price,name,recipe}=item;
+
+    const handleAddToCart = food =>
+    {
+      console.log(food);
+    }
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
   <figure><img src={image} alt="Shoes" /></figure>
@@ -12,7 +18,7 @@ const FoodCard = ({item}) => {
     <h2 className="card-title">{name}</h2>
     <p>{recipe}</p>
     <div className="card-actions justify-end">
-      <button className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add to Cart</button>
+      <button onClick={()=>handleAddToCart(item)} className="btn btn-outline bg-slate-100 border-orange-400 border-0 border-b-4 mt-4">Add to Cart</button>
     </div>
   </div>
 </div>
--- src/pages/Shared/NavBar/NavBar.jsx
+++ src/pages/Shared/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { AuthCon } from "../../../providers/AuthProv";
-
+import { FaShoppingCart } from "react-icons/fa";
 
 const NavBar = () => {
 
@@ -19,7 +19,14 @@ const NavBar = () => {
     <li><Link to="/menu">Our Menu</Link></li>
     <li><Link to="/order/salad">Order Food</Link></li>
     <li><Link to="/secret">Secret</Link></li>
-    
+    <li>
+            <Link to="/">
+                <button className="btn">
+                    <FaShoppingCart className="mr-2"></FaShoppingCart>
+                    <div className="badge badge-secondary">+0</div>
+                </button>
+            </Link>
+        </li>
     {
             user ? <>
                 {/* <span>{user?.displayName}</span> */}
