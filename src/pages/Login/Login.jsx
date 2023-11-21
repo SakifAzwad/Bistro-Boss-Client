a1f77d0fb1d45222254c1d571dbb72b056252e08
SakifAzwad <sakifazwad18@gmail.com>
2023-11-22 04:02:56 +0600

    added private route setups

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -3,7 +3,7 @@ import { useContext } from 'react';
 import { useEffect, useState } from 'react';
 import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
 import { AuthCon } from '../../providers/AuthProv';
-import { Link } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import { Helmet } from 'react-helmet-async';
 import Swal from 'sweetalert2';
 
@@ -13,7 +13,10 @@ const Login = () => {
     const [disabled,setDisabled]=useState(true);
 
     const {signIn}=useContext(AuthCon);
-    
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
     useEffect(()=>
     {
         loadCaptchaEnginge(6);
@@ -38,6 +41,7 @@ const Login = () => {
                   popup: 'animate__animated animate__fadeOutUp'
               }
           });
+           navigate(from, { replace: true });
           })
     }
 
