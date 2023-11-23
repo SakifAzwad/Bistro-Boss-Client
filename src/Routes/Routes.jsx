6470369536c0fa731915502c2fa42f5bc7653b7b
SakifAzwad <sakifazwad18@gmail.com>
2023-11-23 22:41:27 +0600

    Add items from admin panel

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -10,6 +10,8 @@ import Secret from "../pages/Shared/Secret/Secret";
 import Dashboard from "../Layout/Dashboard";
 import Cart from "../pages/Dashboard/Cart/Cart";
 import AllUsers from "../pages/Dashboard/AllUsers/AllUsers";
+import AddItems from "../pages/Dashboard/AddItems/AddItems";
+import AdminRoute from "./AdminRoute";
 
 export const router = createBrowserRouter([
   {
@@ -55,9 +57,13 @@ export const router = createBrowserRouter([
         element:<Cart></Cart>
       },
       {
+        path: "addItems",
+        element:<AdminRoute><AddItems></AddItems></AdminRoute>
+      },
+      {
 
         path: "users",
-        element:<AllUsers></AllUsers>
+        element:<AdminRoute><AllUsers></AllUsers></AdminRoute>
       },
     ],
   },
